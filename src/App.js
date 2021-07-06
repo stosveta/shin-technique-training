a8c3b233c80a44f24e49267f38a6ad9733ab5671
Svitlana Stosyk <stosveta@yahoo.com>
2021-07-06 06:41:33 +0300

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppView from './AppView.js';
    
 function App() {
-  return (
-
-  <AppView />
-
-  );
+  return <AppView />
 }
 
 export default App;
